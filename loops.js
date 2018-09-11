3393d2b752b60dc98faa9849bae9cb48e6e525f7
Loree Nichols <loree.d.nichols@gmail.com>
2018-09-11 02:54:50 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -4,7 +4,7 @@ for (var i = 0; i < 25; i++) {
   if (i === 1) {
     array.push('I am 1 strange loop.')
   } else {
-    array.push('I am ${i} strange loops.')
+    array.push("I am ${i} strange loops.")
   }
   
 }
